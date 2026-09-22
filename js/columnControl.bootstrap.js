9258a64bf205fafadbaa899d58d33d4090649376
dtbuild <dtbuild@rigil>
2026-09-22 09:36:04 +0000

    c0f2200580a23bb7c25cf7bc37219496d5a0530d Release 2.1.0
    Sync to source repo @c0f2200580a23bb7c25cf7bc37219496d5a0530d

--- js/columnControl.bootstrap.js
+++ js/columnControl.bootstrap.js
@@ -1,4 +1,4 @@
-/*! ColumnControl Bootstrap 3 styling 2.0.2 for DataTables
+/*! ColumnControl Bootstrap 3 styling 2.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
